54c82bd0afc2aa7eb690b98fe24e7d2c6db22435
IanPedroBorges <ianhudback@gmail.com>
2024-01-11 17:28:34 -0300

    configurando cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const routers = require('./routes');
+
+const corsConfig = {
+    origin: '*',
+    optionsSuccessStatus: 200
+};
 
 const app = express();
 
+app.use(cors(corsConfig));
 app.use(express.json());
+
 
 app.use(routers);
 
