56fa770e99ee2e79160f5b8576798030e4db5b31
IanPedroBorges <ianhudback@gmail.com>
2024-01-11 22:10:20 -0300

    connection back-end and front-end suceffull

--- back-end/src/models/drinksModel.js
+++ back-end/src/models/drinksModel.js
@@ -1,25 +1,35 @@
+const axios = require('axios');
+
 const getAllDrinks = async () => {
-    const drinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
-    const drinksJson = await drinks.json();
-    return drinksJson;
+    const drinks = await axios({
+        method: 'get',
+        url: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
+    });
+    return drinks.data;
 };
 
 const getDrinksAllCategories = async () => {
-    const drinks = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
-    const drinksJson = await drinks.json();
-    return drinksJson;
+    const drinks = await axios({
+        method: 'get',
+        url: 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
+    });
+    return drinks.data;
 };
 
 const getDrinksByCategory = async (category) => {
-    const drinks = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
-    const drinksJson = await drinks.json();
-    return drinksJson;
+    const drinks = await axios({
+        method: 'get',
+        url: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`
+    });
+    return drinks.data;
 };
 
 const getDrinkById = async (id) => {
-    const drinks = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-    const drinksJson = await drinks.json();
-    return drinksJson;
+    const drinks = await axios({
+        method: 'get',
+        url: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    });
+    return drinks.data;
 };
 
 module.exports = {
--- back-end/src/models/mealsModel.js
+++ back-end/src/models/mealsModel.js
@@ -1,25 +1,35 @@
+const axios = require('axios');
+
 const getAllMeals = async () => {
-    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=');
-    const mealsJson = await meals.json();
-    return mealsJson;
+    const meals = await axios({
+        method: 'get',
+        url: 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
+    });
+    return meals.data;
 };
 
 const getMealsAllCategories = async () => {
-    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
-    const mealsJson = await meals.json();
-    return mealsJson;
+    const meals = await axios({
+        method: 'get',
+        url: 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'
+    });
+    return meals.data;
 };
 
 const getMealsByCategory = async (category) => {
-    const meals = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
-    const mealsJson = await meals.json();
-    return mealsJson;
+    const meals = await axios({
+        method: 'get',
+        url: `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+    });
+    return meals.data;
 };
 
 const getMealById = async (id) => {
-    const meals = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-    const mealsJson = await meals.json();
-    return mealsJson;
+    const meals = await axios({
+        method: 'get',
+        url: `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    });
+    return meals.data;
 };
 
 module.exports = {
